b8017ab0f0396624628656b1441503d3dc8b5d25
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-03-23 12:17:14 +0000

    tests'

--- packages/script.js
+++ packages/script.js
@@ -1,8 +1,8 @@
 const imageUpload = document.getElementById('file-input');
 Promise.all([
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(start)
 
 async function start() {
